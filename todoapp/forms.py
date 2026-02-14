051c3849c13910b4ea6b39d80409cb1f7ebc760b
Mamurjon <aliev.mamurjon27@gmail.com>
2026-02-14 12:31:13 -0500

    added some design

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -1,8 +1,11 @@
-from django import forms 
-
+from django import forms
 from .models import Todo
-
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
-        fields = ["title", "description" , "priority", "due_date"]
+        fields = ["title", "description", "priority", "due_date"]
+    def __init__(self, *args, **kwargs):
+        
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
